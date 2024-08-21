08bb1c195f313757192bd8e22c7fa3f00ebf9c06
Forshteven <geocinds@mail.ru>
2024-08-22 02:25:59 +0300

    Add files via upload

--- module_2_4.py
+++ module_2_4.py
@@ -1,8 +1,11 @@
-numbers = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 111, 12, 13, 14, 15, 49]
+numbers = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 111, 12, 13, 14, 15, 49, 1]
 primes = []
 not_primes = []
+
 for i in numbers:
-    if i < 4 and i != 1:
+    if i == 1:
+        pass
+    elif i < 4:
         primes.append(i)
     elif i%(i**0.5) == 0:
         not_primes.append(i)
