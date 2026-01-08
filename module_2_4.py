6359146a2f67a123359594a0dec695f1b46e5822
Kosikhin Nikolay <geocinds@mail.ru>
2026-01-08 12:09:47 +0900

    Update module_2_4.py

--- module_2_4.py
+++ module_2_4.py
@@ -1,18 +1,18 @@
-numbers = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 111, 12, 13, 14, 15, 49, 1]
+list_1 = [1, 2, 3, 8, 5, 11, 12, 13, 19, 23, 25, 81, 117, 97, 43, 47, 53, 59]
 primes = []
 not_primes = []
-
-for i in numbers:
-    if i == 1:
-        pass
-    elif i < 4:
-        primes.append(i)
-    elif i%(i**0.5) == 0:
+for i in list_1:
+    if i < 2:
         not_primes.append(i)
-    elif i % 2 == 0 or i % 3 == 0:
+    elif i%2 == 0 and i != 2:
+        not_primes.append(i)
+    elif i%3 == 0 and i != 3:
+        not_primes.append(i)
+    elif i%i**0.5 == 0:
         not_primes.append(i)
     else:
         primes.append(i)
+
+
 print(primes)
 print(not_primes)
-
